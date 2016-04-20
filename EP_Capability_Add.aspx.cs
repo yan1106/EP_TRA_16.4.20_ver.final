5ef847ef7999d93b3496f42957c0cf144b74275b
yan1106 <brunohuang1106@gmail.com>
2016-04-20 18:11:44 +0800

    Added files via upload

---
+++ EP_Capability_Add.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class EP_Capability_Add : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!Page.IsPostBack)
+        {
+            Panel_CAP_table.Visible = false;
+        }
+    }
+
+    protected void butSearch_Click(object sender, EventArgs e)
+    {
+
+    }
+}
